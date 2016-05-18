c3b161c20cf64d06c196a02a33e04b98ff00fedc
Émerson Oliveira Silva <emerson.silva@codeminer42.com>
2016-05-18 14:51:55 -0300

    Adiciona gem 'dotenv-rails' e cria arquivo '.env.sample'

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 module AdtsysApplicantTest
   class Application < Rails::Application
